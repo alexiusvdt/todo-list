ba29e86bf036cbe4b27f90f445b84078a8a6fe2a
Alex Johnson <alex.johnson293@gmail.com>
2022-12-08 16:25:24 -0800

    add additional testing/checking

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,9 +1,26 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
-  public class Items
+  public class Item
   {
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
 
-    // properties, methods, etc. will go here.
-
-  }
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
+    //we call this with Item.GetAll() as we target the entire class
+    public static List<Item> GetAll() 
+    {
+      return _instances;
+    }
+    // Add function to clear data for fresh test cycling
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+}
 }
